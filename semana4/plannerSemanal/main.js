9207c182bbf19bde1ce23d14324abaab0a791da5
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-10 13:00:12 -0300

    Alteração no main.js

--- semana4/plannerSemanal/main.js
+++ semana4/plannerSemanal/main.js
@@ -1,37 +1,43 @@
 function addTarefa() {
-  
-let select = document.getElementById("selectDay")
-let selectDia = select.value
-let input = document.getElementById("input")
-let inputValue = input.value
+
+  let select = document.getElementById("selectDay")
+  let selectDia = select.value
+  let input = document.getElementById("input")
+  let inputValue = input.value
+  let selectHora = document.getElementById("selectHora").value
+  let horaTarefa = document.getElementById("horaDaTarefa")
+console.log(horaTarefa)
 
 if(selectDia === "Domingo") {
   let diaSem = document.getElementById("domingo")
-  domingo.innerHTML += `<li style="text-align: center">${inputValue}</li>`
-  input.value = ""
+  horaTarefa.innerHTML += `<li style="text-align: center">${selectHora}</li>`
+  diaSem.innerHTML += `<li style="text-align: center"> ${inputValue}</li>`
+  input.value = "" 
 }else if (selectDia === "Segunda") {
   let diaSem = document.getElementById("segunda")
-  diaSem.innerHTML += `<li style="text-align: center">${inputValue}</li>`
+  diaSem.innerHTML += `<li style="text-align: center">${selectHora} ${inputValue}</li>`
   input.value = ""
 }else if (selectDia === "Terça") {
   let diaSem = document.getElementById("terca")
-  diaSem.innerHTML += `<li style="text-align: center">${inputValue}</li>`
+  diaSem.innerHTML += `<li style="text-align: center">${selectHora} ${inputValue}</li>`
   input.value = ""
 }else if (selectDia === "Quarta") {
   let diaSem = document.getElementById("quarta")
-  diaSem.innerHTML += `<li style="text-align: center">${inputValue}</li>`
+  diaSem.innerHTML += `<li style="text-align: center">${selectHora} ${inputValue}</li>`
   input.value = ""
 }else if (selectDia === "Quinta") {
   let diaSem = document.getElementById("quinta")
-  diaSem.innerHTML += `<li style="text-align: center">${inputValue}</li>`
+  diaSem.innerHTML += `<li style="text-align: center">${selectHora} ${inputValue}</li>`
   input.value = ""
 }else if (selectDia === "Sexta") {
   let diaSem = document.getElementById("sexta")
   diaSem.innerHTML += `<li style="text-align: center">${inputValue}</li>`
   input.value = ""
-}else {
+}else if (selectDia ==="Sabado"){
   let diaSem = document.getElementById("sabado")
   diaSem.innerHTML += `<li style="text-align: center">${inputValue}</li>`
   input.value = ""
 }
 }
+
+
