b3574c0575c7a2b9d60397253ebc82f338290f36
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-12 10:56:18 -0300

    Feito exercício 1 dos exercícios de interpretação de código

---
+++ semana4/lista-de-exercicios/main.js
@@ -0,0 +1,9 @@
+// Exercícios de interpretação de código
+
+/*
+1-
+Função criada para converter o dólar para real, inicia com a função declarada passando por parametro valorEmDolar. 
+A constante cotacao recebe o valor de cotação do dólar do usuario, e a função retorna a multiplicação do valorEmDolar e a cotação.
+A função é chamada através da constante meuDinheiro que recebe a função com o valor do parametro e imprime no console o resultado.
+
+*/
