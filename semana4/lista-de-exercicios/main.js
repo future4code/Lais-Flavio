18f6814cb2df474f566b0d0459d737ff225f1380
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-12 11:04:05 -0300

    Feito exercício 2 dos exercícios de interpretação de código

--- semana4/lista-de-exercicios/main.js
+++ semana4/lista-de-exercicios/main.js
@@ -6,4 +6,11 @@ Função criada para converter o dólar para real, inicia com a função declara
 A constante cotacao recebe o valor de cotação do dólar do usuario, e a função retorna a multiplicação do valorEmDolar e a cotação.
 A função é chamada através da constante meuDinheiro que recebe a função com o valor do parametro e imprime no console o resultado.
 
+2-
+Função criada para tratar os tipos de investimento retornando qual rendimento terá dependendo do tipo de investimento.
+Inicia-se declarando a função e passando 2 parâmetros. É criada uma variável que receberá o resultado após o investimento.
+É criado uma estrutura condicional com switch/case recebendo tipoDeInvestimento como parâmetro e é verificado se é poupança, renda fixa, CDB ou Ações.
+Cada tipo multiplica valor do investimento por seu valor de rendimento. Caso satisfaça uma das opções, a verificação pára.
+Caso não satisfaça nenhuma delas, uma  mensagem de alerta é acionada exibindo tipo incorreto.  A função retorna a variavel valorAposInvestimento.
+São declaradas 2 constantes que recebem e chamam a função com seu tipo de investimento e o valor investido e é exibido no console o rendimento.
 */
