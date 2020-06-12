b9c8d172344b7bb968e9cca59a79f9186727247c
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-12 14:51:31 -0300

    Feito exercício 3:a dos exercícios de funções de array

--- semana4/lista-de-exercicios/main.js
+++ semana4/lista-de-exercicios/main.js
@@ -270,6 +270,30 @@ const multiplicaArray = () => {
 
 multiplicaArray()
 
+3-
+a:
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const adultos = []
+
+const novoArray = () => {
+  
+  for(pessoa of pessoas ) {
+    if((pessoa.idade >= 14) && (pessoa.altura >= 1.5) && (pessoa.idade < 60)) {
+    adultos.push(pessoa)
+    console.log(adultos)
+    }
+  }
+}
+novoArray()
+
 */
 
 
