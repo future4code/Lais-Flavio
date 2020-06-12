e5a7a90f8d885a965a6311f2552f8e074abb2df8
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-12 11:26:54 -0300

    Feito exercício 4 dos exercícios de interpretação de código

--- semana4/lista-de-exercicios/main.js
+++ semana4/lista-de-exercicios/main.js
@@ -19,4 +19,9 @@ Criado 3 arrays, sendo 2 vazios. O primeiro contém os numeros que serão verifi
 É criado um loop para percorrer o array e caso o módulo do numero dividido por 2 seja igual a 0, é enviado para o array1 por que é par,
 caso não seja, é enviado para o array2 por ser ímpar. No console é exibido 3 mensagens: A primeira exibe o total de números no array usando .length
 A segunda mensagem exibe a quantidade de numeros pares no array1 e a terceira no array2 que possui os números ímpares.
+
+4-
+É percorrido o array numeros com for of, onde caso numero do array seja menor que o numero contido na variavel numero1 que tem o valor infinito, essa variável recebe o valor de numero,
+e caso numero seja maior que o numero 0 da variavel numero2, numero2 recebe o valor de número. As mensagens que seriam impressas no console seriam:
+numero1 receberá o valor de -10 que é o menor numero do array. A variavel numero2 receberá 1590 que é o maior numero do array.
 */
