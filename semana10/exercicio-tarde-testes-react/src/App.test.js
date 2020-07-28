3d4a91193676fa2feacc483beef7682ebfefde66
laisflaviosilva <laisflavio.silva@gmail.com>
2020-07-28 16:17:02 -0300

    Não consegui fazer os testes, não entendi muito bem o tema de hoje

--- semana10/exercicio-tarde-testes-react/src/App.test.js
+++ semana10/exercicio-tarde-testes-react/src/App.test.js
@@ -1,12 +1,81 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, getByText } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"
 import App from "./App";
+import Post from './components/Post'
 
 
 describe('Renderização inicial', () => {
+
   test('Input deve aparecer na tela', () => {
     const {getByPlaceholderText} = render(<App />)
-    expect(getByPlaceholderText('Novo post'))
+
+    const input = getByPlaceholderText('Novo post')
+    expect(input).toBeInTheDocument()
+  })
+
+  test('Botão deve aparecer na tela', () => {
+    const {getByText} = render(<App />)
+
+    expect(getByText(/Adicionar/)).toBeInTheDocument()
   })
 })
+
+describe('Ao criar uma tarefa', () => {
+  test('ela deve aparecer na tela, com o mesmo texto digitado no input', () => {
+    const {getByPlaceholderText} = render(<App />)
+    const input = getByPlaceholderText('Novo post')
+    fireEvent.change(input, {
+      target: {
+        value: 'Tarefa de teste'
+    }})
+    expect(input).toHaveValue('Tarefa de teste')
+  })
+
+  test('quando o usuario digita e clica em criar tarefa', () => {
+    const {getByPlaceholderText, getByText} = render(<App />)
+    const input = getByPlaceholderText('Novo post')
+    fireEvent.change(input, {
+      target: {
+        value: 'Tarefa de teste'
+    }})
+
+    const button = getByText(/Adicionar/)
+    fireEvent.click(button)
+
+    expect(getByText(/Tarefa de teste/)).toBeInTheDocument()
+
+  })
+
+  test('quando o usuario digita e clica em criar tarefa, o input deve ser limpo', () => {
+    const {getByPlaceholderText, getByText} = render(<App />)
+    const input = getByPlaceholderText('Novo post')
+    fireEvent.change(input, {
+      target: {
+        value: 'Tarefa de teste'
+    }})
+
+    const button = getByText(/Adicionar/)
+    fireEvent.click(button)
+
+    expect(input).toHaveValue('')
+
+  })
+})
+
+
+describe('Apaga post', () => {
+  test('deve remover post adicionado', () => {
+    const {getByPlaceholderText, getByText} = render(<App />)
+    const button = getByText(/Adicionar/)
+    const input = getByPlaceholderText('Novo post')
+    fireEvent.change(input, {
+      target: {
+        value: 'Tarefa de teste'
+    }})
+    fireEvent.click(button)
+    fireEvent.click(getByText('Apagar'))
+    expect(getByText('Apagar')).toHaveValue('')
+  })
+})
+
