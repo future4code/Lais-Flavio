affb77193cdccb69eff982bb03c7b57c283b1b88
laisflaviosilva <laisflavio.silva@gmail.com>
2020-07-28 13:40:20 -0300

    Inicio dos exercícios

--- semana10/exercicio-tarde-testes-react/src/App.test.js
+++ semana10/exercicio-tarde-testes-react/src/App.test.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"
 import App from "./App";
+
+
+describe('Renderização inicial', () => {
+  test('Input deve aparecer na tela', () => {
+    const {getByPlaceholderText} = render(<App />)
+    expect(getByPlaceholderText('Novo post'))
+  })
+})
